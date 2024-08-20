9fef25baf3b52f23995ad80a5edce3e8f2a550d7
ShaileshIshere <markvaldoz009@gmail.com>
2024-08-20 19:12:32 +0530

    added some responsiveness

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -9,7 +9,6 @@ const wallet = () => {
       <div className="max-w-7xl mx-auto flex flex-col gap-4 p-4 min-h-[94vh] dark:bg-[#0A0A0A]">
         <Navbar />
         <MnemonicDisplay />
-
         <AddWallet />
       </div>
     </>
